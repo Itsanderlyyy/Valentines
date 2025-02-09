3da957e5be643f34829e7e84dd3d5f3e5bef2f6d
Itsanderlyyy <anderlyavila12526@gmail.com>
2025-02-09 19:41:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dcfusyz33/video/upload/v1739126088/SpotifyMate.com_-_For_the_First_Time_-_Mac_DeMarco_sw0pnh.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dcfusyz33/video/upload/v1739126029/SpotifyMate.com_-_For_the_First_Time_-_Mac_DeMarco_xxxu9w.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
